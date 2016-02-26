88bc058328dd02744d1b6cf14a51214f67f10400
JaySonGD <czljcb@163.com>
2016-02-26 19:59:23 +0800

    ++

--- Swift - 闭包/Swift - 闭包/HTTPTool.swift
+++ Swift - 闭包/Swift - 闭包/HTTPTool.swift
@@ -11,7 +11,7 @@ import UIKit
 class HTTPTool: NSObject {
     
     var url :String?
-    var myBlock : ((String) -> ())?
+    var myBlock : ( (String) -> () )?
 
     
     func load(myBlock :(String) -> ()){
--- Swift - 闭包/Swift - 闭包/ViewController.swift
+++ Swift - 闭包/Swift - 闭包/ViewController.swift
@@ -9,62 +9,77 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var tool : HTTPTool?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-       let tool = HTTPTool()
+        let tool = HTTPTool()
         
-//        tool.url = "223"
-//        tool.load { (data : String) -> () in
-//            
-//            print(data)
-//        }
-//        
-//        tool.load ({ (data : String) -> () in
-//            
-//            print("888\(data)")
-//        })
+        self.tool = tool
+        
+        // MARK: - 闭包格式      (参数类型) -> (返回值类型)
+        // var myBlock : ( (String) -> () )?
+
+        
+        // MARK: - 常用写法
+        //        tool.load { (data : String) -> () in
+        //
+        //            print(data)
+        //        }
+        //
+        // MARK: - 方式2
+        
+        //        tool.load ({ (data : String) -> () in
+        //
+        //            print("888\(data)")
+        //        })
+        
+        // MARK: - 方式3
+        
+        //        tool.load (){ (data : String) -> () in
+        //            print("9999\(data)")
+        //            self.view.backgroundColor = UIColor.orangeColor()
+        //        }
+        
+        // MARK: - 最直接方式
+        
+        //        weak var weakSelf : ViewController? = self
+        //
+        //
+        //        tool.load (){ (data : String) -> () in
+        //            print("9999\(data)")
+        //            weakSelf!.view.backgroundColor = UIColor.orangeColor()
+        //        }
         
         
-//        weak var weakSelf : ViewController? = self
-//        
-//        
-//        tool.load (){ (data : String) -> () in
-//            print("9999\(data)")
-//            weakSelf!.view.backgroundColor = UIColor.orangeColor()
-//        }
+        // MARK: - 最直接方式（简写）
         
-        
-//        
-//                tool.load (){[weak self] (data : String) -> () in
-//                    print("9999\(data)")
-//                    self!.view.backgroundColor = UIColor.orangeColor()
-//                }
+        //                tool.load (){[weak self] (data : String) -> () in
+        //                    print("9999\(data)")
+        //                    self!.view.backgroundColor = UIColor.orangeColor()
+        //                }
         
         
         
+        // MARK: - 不安全方式（viewController delloc 时 指针不清空，永远指着那一块地址）
         
         tool.load (){[unowned self] (data : String) -> () in
             print("9999\(data)")
             self.view.backgroundColor = UIColor.orangeColor()
         }
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     
+    
+    // MARK: - 相当于OC delloc
     deinit{
-       print("delloc")
+        print("delloc")
     }
-
-
+    
+    
 }
 
